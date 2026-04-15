eb0fc9e8879935258ae5a1c9103df9c38968af00
sejalkadam28 <kadamsejal28@gmail.com>
2026-04-15 10:36:05 +0530

    Add files via upload

---
+++ strcontains.java
@@ -0,0 +1,10 @@
+class stest
+{
+public static void main(String [] args)
+{
+String s="sunday la kay plan aahe bro";
+
+System.out.println(s.contains("plan"));
+System.out.println(s.contains("bhava"));
+}
+}
